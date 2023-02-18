2dc4f88bb979542bae7b08dfd1e37e11f7c6d092
Владимир Павловский <pavlovskiy.v.v@yandex.ru>
2023-02-18 22:11:11 +0300

    Приложение Users. Переопределение шаблона logged_out

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -31,3 +31,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-pub_date"]
+
+    def __str__(self):
+        # выводим текст поста 
+        return self.text 
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
